25a9d5463d1daad019fb7e3b383430a89f197b24
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-07-19 13:26:19 +0100

    Update sript.R

--- sript.R
+++ sript.R
@@ -1,4 +1,4 @@
-           ################## Requiered packages ##########################################
+              ################## Requiered packages ##########################################
      
       library(dplyr)
       library(Seurat)
@@ -23,6 +23,45 @@
       FeatureScatter(object = mix_1, feature1 = "nCount_RNA", feature2 = "nFeature_RNA",pt.size = 1,smooth = T)
      
       rm(RO48_1,DMSO_1)#remove objects to reduce RAM load
+        
+        ######################### PCA exp_1 #################################################
+        
+        all.genes <- rownames(x = mix_1)
+        
+        mix_1 <- ScaleData(object = mix_1, features = all.genes)
+        mix_1 <- RunPCA(object = mix_1, features = VariableFeatures(object = mix_1))
+        
+        VizDimLoadings(object = mix_1, dims = 1:2, reduction = 'pca')
+        DimPlot(object = mix_1, reduction = 'pca')
+        
+        DimHeatmap(object =mix_1, dims = 1, cells = 500, balanced = TRUE)
+        DimHeatmap(object =mix_1, dims = 1:15, cells = 500, balanced = TRUE)
+        
+        ElbowPlot(object = mix_1)
+        
+        ################### Clusters exp_1 ################################################
+        
+        mix_1 <- FindNeighbors(object = mix_1, dims = 1:10)
+        mix_1 <- FindClusters(object = mix_1, resolution = 0.5)
+        
+        ####################### UMAP exp_1 #######################################################
+        #pip install umap-learn
+      
+      mix_1 <- RunUMAP(object = mix_1, dims = 1:10)
+      DimPlot(object = mix_1, reduction = 'umap')
+      
+      ######################### cluster biomarkers exp_1 #######################################
+      
+      # find all markers of cluster 1
+      cluster1.markers <- FindMarkers(object = mix_1, ident.1 = 1, min.pct = 0.25)
+      head(x = cluster1.markers, n = 5)
+      # find all markers distinguishing cluster 5 from clusters 0 and 3
+      cluster5.markers <- FindMarkers(object = mix_1, ident.1 = 5, ident.2 = c(0, 3), min.pct = 0.25)
+      head(x = cluster5.markers, n = 5)
+      # find markers for every cluster compared to all remaining cells, report only the positive ones
+      pbmc.markers <- FindAllMarkers(object = mix_1, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+      pbmc.markers %>% group_by(cluster) %>% top_n(n = 2, wt = avg_logFC)
+      
    
     ######################## Exp_2 ##################################################
    
@@ -44,44 +83,7 @@
       
     rm(RO48_2,DMSO_2)
     
-   ######################### PCA exp_1 #################################################
   
-    all.genes <- rownames(x = mix_1)
-    
-    mix_1 <- ScaleData(object = mix_1, features = all.genes)
-    mix_1 <- RunPCA(object = mix_1, features = VariableFeatures(object = mix_1))
-
-    VizDimLoadings(object = mix_1, dims = 1:2, reduction = 'pca')
-    DimPlot(object = mix_1, reduction = 'pca')
-    
-    DimHeatmap(object =mix_1, dims = 1, cells = 500, balanced = TRUE)
-    DimHeatmap(object =mix_1, dims = 1:15, cells = 500, balanced = TRUE)
-    
-    ElbowPlot(object = mix_1)
-    
-    ################### Clusters exp_1 ################################################
-   
-    mix_1 <- FindNeighbors(object = mix_1, dims = 1:10)
-    mix_1 <- FindClusters(object = mix_1, resolution = 0.5)
-    
-    ####################### UMAP exp_1 #######################################################
-    #pip install umap-learn
-    
-    mix_1 <- RunUMAP(object = mix_1, dims = 1:10)
-    DimPlot(object = mix_1, reduction = 'umap')
-    
-    ######################### cluster biomarkers exp_1 #######################################
-    
-    # find all markers of cluster 1
-    cluster1.markers <- FindMarkers(object = mix_1, ident.1 = 1, min.pct = 0.25)
-    head(x = cluster1.markers, n = 5)
-    # find all markers distinguishing cluster 5 from clusters 0 and 3
-    cluster5.markers <- FindMarkers(object = mix_1, ident.1 = 5, ident.2 = c(0, 3), min.pct = 0.25)
-    head(x = cluster5.markers, n = 5)
-    # find markers for every cluster compared to all remaining cells, report only the positive ones
-    pbmc.markers <- FindAllMarkers(object = mix_1, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
-    pbmc.markers %>% group_by(cluster) %>% top_n(n = 2, wt = avg_logFC)
-    
     
     
     ######################### PCA exp_2 #################################################
@@ -122,4 +124,64 @@
     pbmc.markers %>% group_by(cluster) %>% top_n(n = 2, wt = avg_logFC)
     
     
+    ##################################################################################
+    ###################### Full_data #################################################
+    ##################################################################################
+    rm(mix_1,mix_2)
     
+    RO48_1 <- Read10X(data.dir = "~/Dropbox/DataScience/Fiver/RO48_1/")%>%
+      CreateSeuratObject(min.cells = 30, min.features = 2000,project = "RO48 Exp.1")
+    DMSO_1 <- Read10X(data.dir = "~/Dropbox/DataScience/Fiver/DMSO_1/")%>%
+      CreateSeuratObject(min.cells = 30, min.features = 2000,project = "DMSO Exp.1")
+    
+    mix_1 <- merge(x=RO48_1,y= DMSO_1)
+    rm(RO48_1,DMSO_1)
+    
+    RO48_2 <- Read10X(data.dir = "~/Dropbox/DataScience/Fiver/RO48_2/")%>%
+      CreateSeuratObject(min.cells = 30, min.features = 2000,project = "RO48 Exp.2")
+    DMSO_2 <- Read10X(data.dir = "~/Dropbox/DataScience/Fiver/DMSO_2/")%>%
+      CreateSeuratObject(min.cells = 30, min.features = 2000,project = "DMSO Exp.2")
+    
+    
+    mix_2 <- merge(x=RO48_2,y= DMSO_2)
+    rm(RO48_2,DMSO_2)
+    
+    mix_full <- merge(x=mix_1,y=mix_2)
+    
+    rm (mix_1,mix_2)
+    
+    ##################################################################################
+    
+    FeatureScatter(object = mix_full, feature1 = "nCount_RNA", feature2 = "nFeature_RNA",pt.size = 1,smooth = F)
+    
+    ######################### PCA exp_full #################################################
+    
+    all.genes <- rownames(x = mix_full)
+    
+    mix_full <- ScaleData(object = mix_full, features = all.genes)
+    mix_full <- RunPCA(object = mix_full, features = VariableFeatures(object = mix_full))
+    
+    VizDimLoadings(object = mix_full, dims = 1:2, reduction = 'pca')
+    DimPlot(object = mix_full, reduction = 'pca')
+    
+    DimHeatmap(object =mix_full, dims = 1, cells = 500, balanced = TRUE)
+    DimHeatmap(object =mix_full, dims = 1:15, cells = 500, balanced = TRUE)
+    
+    ElbowPlot(object = mix_full)
+    
+    ################### Clusters exp_full ################################################
+    
+    mix_full <- FindNeighbors(object = mix_full, dims = 1:10)
+    mix_full <- FindClusters(object = mix_full, resolution = 0.5)
+    
+    ####################### UMAP exp_full #######################################################
+    #Need to install pip on Mac and run in console: sudo apt-get install pip 
+    #Install the package with: pip install umap-learn
+    
+    
+    mix_full <- RunUMAP(object = mix_full, dims = 1:10)
+    DimPlot(object = mix_full, reduction = 'umap')
+    
+    
+    ###########################################################################################
+  
